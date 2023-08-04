f9fa18c62de5b6a8e3bf0033eaa0c3fc60756a38
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-08-04 23:33:41 +0900

    固定ページ設置

--- app/src/main/java/com/tfandkusu/tpic/MainScreen.kt
+++ app/src/main/java/com/tfandkusu/tpic/MainScreen.kt
@@ -1,7 +1,13 @@
 package com.tfandkusu.tpic
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -12,11 +18,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.tfandkusu.tpic.ui.theme.MyTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun MainScreen() {
+    val pagerState = rememberPagerState()
     Scaffold(
         topBar = {
             TopAppBar(
@@ -30,9 +38,25 @@ fun MainScreen() {
             )
         },
     ) { padding ->
-        Box(
-           modifier = Modifier.padding(padding)
-        )
+        HorizontalPager(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxWidth(),
+            state = pagerState,
+            pageCount = 3,
+            reverseLayout = true
+        ) { page ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "2023/%02d".format(3 - page),
+                    style = MaterialTheme.typography.headlineLarge,
+                )
+            }
+        }
     }
 }
 
